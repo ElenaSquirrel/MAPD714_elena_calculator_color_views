a0f0263051f767977d4e6928ec37149aca2889e0
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-10-11 14:43:19 -0400

    SwitchingViewController.swift : switchView method with animations added; switchViewController method (set background color, set text color) added

--- MAPD714_ElenaMelnikova_optionsScreen/SwitchingViewController.swift
+++ MAPD714_ElenaMelnikova_optionsScreen/SwitchingViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class SwitchingViewController: UIViewController {
+    private var calculatorViewController: CalculatorViewController!
+    private var optionsViewController: OptionsViewController!
    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        calculatorViewController = storyboard?.instantiateViewController(withIdentifier: "Calculator") as! CalculatorViewController
+        calculatorViewController.view.frame = view.frame
+        switchViewController(from: nil, to: calculatorViewController)
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +35,103 @@ class SwitchingViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+        // Create the new view controller, if required
+        if optionsViewController?.view.superview == nil {
+            if optionsViewController == nil {
+                optionsViewController =
+                    storyboard?.instantiateViewController(withIdentifier: "Options")
+                    as! OptionsViewController
+            }
+        } else if calculatorViewController?.view.superview == nil {
+            if calculatorViewController == nil {
+                calculatorViewController =
+                    storyboard?.instantiateViewController(withIdentifier: "Calculator")
+                    as! CalculatorViewController
+            }
+        }
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        // Switch view controllers
+        if calculatorViewController != nil
+            && calculatorViewController!.view.superview != nil {
+            UIView.setAnimationTransition(.flipFromRight,
+                               for: view, cache: true)
+            optionsViewController.view.frame = view.frame
+            switchViewController(from: calculatorViewController,
+                to: optionsViewController)
+        } else {
+            UIView.setAnimationTransition(.flipFromLeft,
+                                          for: view, cache: true)
+            calculatorViewController.view.frame = view.frame
+            switchViewController(from: optionsViewController,
+                                 to: calculatorViewController)
+        }
+        UIView.commitAnimations()
+    }
 
+    private func switchViewController(from fromVC:UIViewController?,
+                     to toVC:UIViewController?) {
+        var textColor = UIColor.white
+        var backgroungColor = UIColor.gray
+        var tColor = "White"
+        var bkColor = "Gray"
+ 
+        if fromVC != nil {
+            fromVC!.willMove(toParentViewController: nil)
+            fromVC!.view.removeFromSuperview()
+            fromVC!.removeFromParentViewController()
+
+            if optionsViewController.menuOutlet.titleLabel != nil {
+                if optionsViewController.menuOutlet.titleLabel!.text != nil {
+                    tColor = optionsViewController.menuOutlet.titleLabel!.text!
+                    switch tColor {
+                    case "Yellow":
+                        textColor = UIColor.yellow
+                        break
+                    default:
+                        textColor = UIColor.white
+                    }
+                }
+            }
+  
+            if optionsViewController.menuOutlet2.titleLabel != nil {
+                if optionsViewController.menuOutlet2.titleLabel!.text != nil {
+                    bkColor = optionsViewController.menuOutlet2.titleLabel!.text!
+                    switch bkColor {
+                    case "Red":
+                        backgroungColor = UIColor.red
+                        break
+                    default:
+                        backgroungColor = UIColor.gray
+                    }
+                }
+            }
+        }
+        if toVC != nil {
+            self.addChildViewController(toVC!)
+            self.view.insertSubview(toVC!.view, at: 0)
+            toVC!.didMove(toParentViewController: self)
+       
+            let buttons = getSubviewsOfView(v: calculatorViewController.view)
+            for btn in buttons {
+                btn.setTitleColor(textColor, for: .normal)
+                if btn.titleLabel?.text != "C" && btn.titleLabel?.text != "=" {
+                    btn.backgroundColor = backgroungColor
+                }
+            }
+        }
+    }
+
+    func getSubviewsOfView(v:UIView) -> [UIButton] {
+        var buttonArray = [UIButton]()
+        for subview in v.subviews {
+            buttonArray += getSubviewsOfView(v: subview)
+            if subview is UIButton {
+                buttonArray.append(subview as! UIButton)
+            }
+        }
+        return buttonArray
+    }
 }
